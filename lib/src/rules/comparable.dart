30d406eefbf4ede67bd94364709c4d05a0ee726c
Albert221 <w.albert221@gmail.com>
2020-10-23 21:10:39 +0200

    Allow null values in SortedComparable

--- lib/src/rules/comparable.dart
+++ lib/src/rules/comparable.dart
@@ -3,5 +3,10 @@ import 'comparator.dart';
 
 class SortedComparable<T, P extends Comparable> extends SortedComparator<T, P> {
   SortedComparable(SortedMapper<T, P> mapper, {bool invert = false})
-      : super(mapper, Comparable.compare, invert: invert);
+      : super(mapper, (a, b) {
+          if (a == null && b == null) return 0;
+          if (a == null) return -1;
+          if (b == null) return 1;
+          return Comparable.compare(a, b);
+        }, invert: invert);
 }
